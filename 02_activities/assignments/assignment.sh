50e54326d6294520361df673cafac74fc0fe208d
Daniel Mori-Fegan <daniel.mori.fegan@mail.utoronto.ca>
2025-01-14 18:07:34 -0500

    Fixed question 7

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -37,8 +37,8 @@ cp raw/server_*.log* processed/server_logs
 cp raw/user_*.log* processed/user_logs
 cp raw/event_*.log* processed/event_logs
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-rm -iv raw/*ipaddr*
-rm -iv processed/user_logs/*ipaddr*
+rm  raw/*ipaddr*
+rm  processed/user_logs/*ipaddr*
 #rf -rf ./data not added as this is results in an error. if rm -rf ./data, this is also not accepted as step 8 would not be possible
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
